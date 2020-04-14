f0c83c23ed56f043e836c793b645b8f7d2a0ed51
Stanislav Kaleta <kstanleykale@gmail.com>
2020-04-14 16:41:11 +0200

    Implemented swipe menu and improved icon-buttons

--- app/src/main/java/cz/muni/fi/pv239/gtodolist/ui/RecyclerViewClickListener.kt
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pv239.gtodolist.ui
-
-import android.view.View
-
-interface RecyclerViewClickListener {
-    fun recyclerViewListClicked(v: View?, position: Int)
-}
--- app/src/main/java/cz/muni/fi/pv239/gtodolist/ui/ToDoListFragment.kt
+++ app/src/main/java/cz/muni/fi/pv239/gtodolist/ui/ToDoListFragment.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.baoyz.swipemenulistview.SwipeMenu
+import com.baoyz.swipemenulistview.SwipeMenuListView
 import cz.muni.fi.pv239.gtodolist.R
+import cz.muni.fi.pv239.gtodolist.api.CalendarExporter
 import cz.muni.fi.pv239.gtodolist.api.ToDoService
+import cz.muni.fi.pv239.gtodolist.model.ToDo
 import kotlinx.android.synthetic.main.fragment_todo_list.*
+
 
 /**
  * Fragment that contains list of TODO tasks.
  */
-class ToDoListFragment : Fragment(), RecyclerViewClickListener {
+class ToDoListFragment : Fragment(), SwipeMenuListView.OnMenuItemClickListener, AdapterView.OnItemClickListener  {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +32,28 @@ class ToDoListFragment : Fragment(), RecyclerViewClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        todo_list.adapter = ToDoListAdapter(ToDoService.listAllNotDone(), this)
-        todo_list.layoutManager = LinearLayoutManager(this.context)
+        listView.adapter = ToDoListAdapter(ToDoService.listAllNotDone())
+        listView.setMenuCreator(ToDoSwipeMenu())
+        listView.setOnMenuItemClickListener(this)
+        listView.setOnItemClickListener(this)
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT)
     }
 
-    override fun recyclerViewListClicked(v: View?, position: Int) {
-        val id = (todo_list.adapter as ToDoListAdapter).todos[position].id
+    override fun onMenuItemClick(position: Int, menu: SwipeMenu?, index: Int): Boolean {
+        val todo: ToDo = listView.adapter.getItem(position) as ToDo
+        when (index) {
+            0 -> {
+                startActivity(CalendarExporter.createIntent(todo))
+            }
+            1 -> {
+                ToDoService.markAsDone(todo.id)
+                listView.adapter = ToDoListAdapter(ToDoService.listAllNotDone())
+            }
+        }
+        return false
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val bundle = bundleOf("id" to id)
         findNavController().navigate(R.id.action_to_todo_info, bundle)
     }
